f9675b738a49bce4fed8c52ea7215b7cbc4c70d4
newfarking <1023889361@qq.com>
2017-02-03 19:18:45 +0800

    meihua

--- server/hpels-server/hpels-server-web/src/main/java/com/newfarking/hpels/controller/HomeController.java
+++ server/hpels-server/hpels-server-web/src/main/java/com/newfarking/hpels/controller/HomeController.java
@@ -57,7 +57,7 @@ public class HomeController {
                     "<div style=\"overflow:auto;margin-top:5px\"><div style=\"float:left;overflow:auto;\"> "
                             + index + ". "
                             + username + " <span style=\"color:red\">" + rank.useTime +
-                            "s</span> <a href=\"#\" onclick=\"gameScene._layer.startReplay('" + rank.id + "', true)\" style=\"font-size:6px;\">观看录像</a></div></div>");
+                            "s</span> <a href=\"#\" onclick=\"gameScene._layer.startReplay('" + rank.id + "', true)\" >观看录像</a></div></div>");
             index ++;
         }
         return stringBuilder.toString();
